68526830c888eb97ce322277d790415a5dfaffd9
Janis <janisivanov@icloud.com>
2021-12-05 12:40:49 +0200

    Changes

---
+++ NFTWEBSITE/Pages/BestTokens.cshtml.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace NFTWEBSITE.Pages
+{
+    public class BestTokensModel : PageModel
+    {
+        private readonly ILogger<BestTokensModel> _logger;
+
+        public BestTokensModel(ILogger<BestTokensModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+}
