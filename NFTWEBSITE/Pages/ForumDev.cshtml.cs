c7f52146fec5e756f08ddf73490f914a373481d3
Janis <janisivanov@icloud.com>
2021-12-10 14:01:39 +0200

    changes

---
+++ NFTWEBSITE/Pages/ForumDev.cshtml.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace NFTWEBSITE.Pages
+{
+    public class ForumDevModel : PageModel
+    {
+        private readonly ILogger<ForumDevModel> _logger;
+
+        public ForumDevModel(ILogger<ForumDevModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+}
