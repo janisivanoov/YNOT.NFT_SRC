1346f451e4ded7a1bb5416b09acc33edbf0ef691
Janis <janisivanov@icloud.com>
2021-12-12 18:50:35 +0200

    changes

---
+++ NFTWEBSITE/Pages/OurProjects.cshtml.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace NFTWEBSITE.Pages
+{
+    public class OurProjectsModel : PageModel
+    {
+        private readonly ILogger<OurProjectsModel> _logger;
+
+        public OurProjectsModel(ILogger<OurProjectsModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+}
